4ac02129a13b315cf9318992faf47a4ea548ca3f
tarousinpo <shinpo@miraie-group.jp>
2018-02-26 13:03:37 +0900

    勝敗追加、gitignore削除

--- tarou/ruby_toybox/test.rb
+++ tarou/ruby_toybox/test.rb
@@ -5,6 +5,7 @@ names.each do |name|
   monsters.each do |monster|
     puts "#{name} VS #{monster}"
     result = [name, monster]
-    puts "#{result.sample} WIN!!!"
+    result.shuffle!
+    puts "Winner #{result[0]}   Loser #{result[1]}(◞‸◟)"
   end
 end
