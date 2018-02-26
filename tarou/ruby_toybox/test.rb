a398610bf45bf8cc8fd8d25e88dcd6a58a8132bd
tarousinpo <shinpo@miraie-group.jp>
2018-02-26 12:36:42 +0900

    勝敗を決めるように変更

--- tarou/ruby_toybox/test.rb
+++ tarou/ruby_toybox/test.rb
@@ -4,5 +4,7 @@ monsters = ['GODZILLA', 'KingKong', 'Hedora']
 names.each do |name|
   monsters.each do |monster|
     puts "#{name} VS #{monster}"
+    result = [name, monster]
+    puts "#{result.sample} WIN!!!"
   end
 end
